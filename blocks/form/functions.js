5bdf91241d72074f32e65574429a02fd7448188f
priyankanr <pnr@adobe.com>
2026-05-04 22:07:58 +0530

    css18

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -56,6 +56,67 @@ function maskMobileNumber(mobileNumber) {
   return ` ${'*'.repeat(5)}${value.substring(5)}`;
 }
 
+function initBankUI() {
+  const select = document.querySelector("select[name='salary_bank']");
+  if (!select || select.dataset.init === "true") return;
+
+  select.dataset.init = "true";
+
+  const banks = [
+    ['hdfc_bank','HDFC','/content/dam/pnr-hdfc/hdfc.png'],
+    ['icici_bank','ICICI','/content/dam/pnr-hdfc/icici.png'],
+    ['axis_bank','Axis','/content/dam/pnr-hdfc/axis.png'],
+    ['kotak_bank','Kotak','/content/dam/pnr-hdfc/kotak.png'],
+    ['sbi','SBI','/content/dam/pnr-hdfc/sbi.png'],
+    ['bank_of_baroda','BOB','/content/dam/pnr-hdfc/bob.jpeg'],
+    ['idfc_first_bank','IDFC','/content/dam/pnr-hdfc/idfc.png']
+  ];
+
+  const container = document.createElement("div");
+  container.className = "bank-row";
+
+  banks.forEach(([val,label,img]) => {
+    const item = document.createElement("div");
+    item.className = "bank-logo";
+    item.dataset.value = val;
+
+    item.innerHTML = `<img src="${img}" alt="${label}">`;
+
+    item.onclick = () => {
+      select.value = val;
+      select.dispatchEvent(new Event('change', { bubbles:true }));
+      updateUI();
+    };
+
+    container.appendChild(item);
+  });
+
+  select.parentNode.insertBefore(container, select);
+
+  function updateUI() {
+    const value = select.value || "hdfc_bank";
+
+    // show only HDFC initially
+    container.querySelectorAll(".bank-logo").forEach(el => {
+      if (value === "hdfc_bank") {
+        el.style.display = el.dataset.value === "hdfc_bank" ? "flex" : "none";
+      } else {
+        el.style.display = "flex";
+      }
+
+      el.classList.toggle("active", el.dataset.value === value);
+    });
+  }
+
+  select.addEventListener("change", updateUI);
+
+  updateUI();
+}
+
+// SAFE init for AEM
+if (typeof window !== "undefined") {
+  setTimeout(initBankUI, 1500);
+}
 
 // eslint-disable-next-line import/prefer-default-export
 export {
